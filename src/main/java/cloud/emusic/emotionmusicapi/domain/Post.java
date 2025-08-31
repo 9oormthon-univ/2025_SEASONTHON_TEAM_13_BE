c83a593f0d289917d95600a63c9d2cfbefb2e7f8
tuioe <vlsxm63@gmail.com>
2025-08-31 19:07:43 +0900

    feat: Post 엔티티 구현
    - User, DayTag, PostEmotionTag와 연관관계 매핑
    - orphanRemoval = true 옵션으로 연관 엔티티(PostEmotionTag, DayTag) 자동 삭제 처리

---
+++ src/main/java/cloud/emusic/emotionmusicapi/domain/Post.java
@@ -0,0 +1,38 @@
+package cloud.emusic.emotionmusicapi.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "post")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DayTag> dayTags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostEmotionTag> emotionTags = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
