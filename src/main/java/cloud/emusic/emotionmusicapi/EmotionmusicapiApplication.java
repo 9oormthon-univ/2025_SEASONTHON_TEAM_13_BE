a24b399dfedc6b9bb4365b2a96a8ad8914d5459e
hsx0306 <1317hsb@gmail.com>
2025-08-31 16:22:01 +0900

    feat(test): 애플리케이션 실행 시 테스트 유저 데이터 삽입

--- src/main/java/cloud/emusic/emotionmusicapi/EmotionmusicapiApplication.java
+++ src/main/java/cloud/emusic/emotionmusicapi/EmotionmusicapiApplication.java
@@ -1,14 +1,28 @@
 package cloud.emusic.emotionmusicapi;
 
+import cloud.emusic.emotionmusicapi.service.UserService;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EmotionmusicapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmotionmusicapiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(UserService userService) {
+		return args -> {
+			System.out.println("===== Inserting Test User Data =====");
+			userService.signUp();
+			System.out.println("====================================");
+		};
 	}
 
 }
