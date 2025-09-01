57812df025a32a345c17215a99a9c818656d7a39
tuioe <vlsxm63@gmail.com>
2025-09-01 20:35:38 +0900

    feat: EmotionTagRepository 인터페이스 구현

---
+++ src/main/java/cloud/emusic/emotionmusicapi/repository/EmotionTagRepository.java
@@ -0,0 +1,6 @@
+package cloud.emusic.emotionmusicapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmotionTagRepository extends JpaRepository<EmotionTagRepository, Long> {
+}
