fec14e89b973cba526d8bdd8682e3f59e48e7a4a
tuioe <vlsxm63@gmail.com>
2025-09-01 21:17:18 +0900

    fix: EmotionTagRepository 인터페이스 에러 해결

--- src/main/java/cloud/emusic/emotionmusicapi/repository/EmotionTagRepository.java
+++ src/main/java/cloud/emusic/emotionmusicapi/repository/EmotionTagRepository.java
@@ -1,6 +1,7 @@
 package cloud.emusic.emotionmusicapi.repository;
 
+import cloud.emusic.emotionmusicapi.domain.EmotionTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmotionTagRepository extends JpaRepository<EmotionTagRepository, Long> {
+public interface EmotionTagRepository extends JpaRepository<EmotionTag, Long> {
 }
