3994f60f2f2f82d8e9245a802c9bfc1d496d2f02
tuioe <vlsxm63@gmail.com>
2025-09-01 17:31:17 +0900

    feat: DayTagRepository 인터페이스 구현

---
+++ src/main/java/cloud/emusic/emotionmusicapi/repository/DayTagRepository.java
@@ -0,0 +1,7 @@
+package cloud.emusic.emotionmusicapi.repository;
+
+import cloud.emusic.emotionmusicapi.domain.DayTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DayTagRepository extends JpaRepository<DayTag, Long> {
+}
