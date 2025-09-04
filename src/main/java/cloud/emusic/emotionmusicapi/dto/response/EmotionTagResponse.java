b13e42f1f75ed634ec32ad84e3e3d6d27f35c564
tuioe <vlsxm63@gmail.com>
2025-09-04 17:31:32 +0900

    refactor: DTO 클래스 패키지 구조 정리

--- src/main/java/cloud/emusic/emotionmusicapi/dto/response/EmotionTagResponse.java
+++ src/main/java/cloud/emusic/emotionmusicapi/dto/response/EmotionTagResponse.java
@@ -1,4 +1,4 @@
-package cloud.emusic.emotionmusicapi.dto.request;
+package cloud.emusic.emotionmusicapi.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
