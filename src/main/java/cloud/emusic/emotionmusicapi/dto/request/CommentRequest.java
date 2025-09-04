0886041501ad3e09d110b4ef113ebe8c65714a89
hsx0306 <1317hsb@gmail.com>
2025-09-04 12:28:07 +0900

    feat: Comment DTO 생성 및 Repository 연결

---
+++ src/main/java/cloud/emusic/emotionmusicapi/dto/request/CommentRequest.java
@@ -0,0 +1,12 @@
+package cloud.emusic.emotionmusicapi.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CommentRequest {
+  @NotBlank(message = "댓글 내용은 비워둘 수 없습니다.")
+  private String content;
+}
