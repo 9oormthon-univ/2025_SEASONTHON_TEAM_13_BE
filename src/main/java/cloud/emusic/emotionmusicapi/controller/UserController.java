b783ad8c3e5104063939d1e7ee6e88b66d0c3871
tuioe <vlsxm63@gmail.com>
2025-09-15 23:10:17 +0900

    feat: 유저 API에 @ApiExceptions 기반 Swagger 예외 응답 적용
    - @ApiExceptions 애노테이션을 통해 게시글 관련 API의 예외 코드 선언
    - OperationCustomizer에서 ErrorCode를 읽어 Swagger 문서에 자동 반영
    - ErrorResponse DTO를 활용하여 상태코드별 예시(JSON) 생성

--- src/main/java/cloud/emusic/emotionmusicapi/controller/UserController.java
+++ src/main/java/cloud/emusic/emotionmusicapi/controller/UserController.java
@@ -2,13 +2,14 @@ package cloud.emusic.emotionmusicapi.controller;
 
 import cloud.emusic.emotionmusicapi.dto.response.user.UserInfoResponse;
 import cloud.emusic.emotionmusicapi.dto.response.user.UserStateResponse;
-import cloud.emusic.emotionmusicapi.exception.ErrorResponse;
+import cloud.emusic.emotionmusicapi.exception.ApiExceptions;
 import cloud.emusic.emotionmusicapi.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,10 +17,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static cloud.emusic.emotionmusicapi.exception.dto.ErrorCode.INTERNAL_SERVER_ERROR;
+import static cloud.emusic.emotionmusicapi.exception.dto.ErrorCode.UNAUTHORIZED;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
-@Schema(description = "유저 관련 API")
+@Tag(name = "User API", description = "유저 관련 API 문서")
 public class UserController {
 
     private final UserService userService;
@@ -29,11 +33,8 @@ public class UserController {
             @ApiResponse(responseCode = "200", description = "조회 성공",
                     content = @Content(mediaType = "application/json",
                             schema = @Schema(implementation = UserInfoResponse.class))),
-            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자",
-                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "서버 내부 오류",
-                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
+    @ApiExceptions(values = {UNAUTHORIZED,INTERNAL_SERVER_ERROR})
     @GetMapping("/me")
     public ResponseEntity<UserInfoResponse> getUserInfo(@AuthenticationPrincipal(expression = "id") Long userId) {
         return ResponseEntity.ok(userService.getUserInfo(userId));
@@ -44,11 +45,8 @@ public class UserController {
             @ApiResponse(responseCode = "200", description = "조회 성공",
                     content = @Content(mediaType = "application/json",
                             schema = @Schema(implementation = UserStateResponse.class))),
-            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자",
-                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "서버 내부 오류",
-                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
+    @ApiExceptions(values = {UNAUTHORIZED,INTERNAL_SERVER_ERROR})
     @GetMapping("/state")
     public ResponseEntity<UserStateResponse> getUserState(@AuthenticationPrincipal(expression = "id") Long userId) {
         return ResponseEntity.ok(userService.getUserStatus(userId));
